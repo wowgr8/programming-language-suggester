4efa2596c8a7fbc3c3c28bac280fb5692ddf2879
Cesar Lopez <maclopez88@yahoo.com>
2021-11-06 13:58:05 -0700

    add user interface logic to js. 1st draft- non functional.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,17 @@ $(document).ready(function(event) {
     const quest4 = parseInt($("#pokemon").val());
     const quest5 = parseInt($("#vacation").val());
     const total = quest1 + quest2 + quest3 + quest4 + quest5;
-
-    if(total)
-  })
-})
+console.log("hi");
+    if(total >= 5) {
+      let resultVal = "Python";
+      $("#language-suggested").text(resultVal);
+    } else if (total <= 5 && 10) {
+      let resultVal = "Swift";
+      $("#language-suggested").text(resultVal);
+    } else {
+      let resultVal = "Ruby";
+      $("#language-suggested").text(resultVal);
+    }
+    $("#result").show();
+  });
+});
