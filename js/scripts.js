da965908691d312cf38e346ab9cb861a1951a3cb
Cesar Lopez <maclopez88@yahoo.com>
2021-11-06 13:05:41 -0700

    add business logic 1st draft in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-$
+$(document).ready(function(event) {
+  $(".questions").submit(function(event) {
+    event.preventDefault();
+    const quest1 = parseInt($("#environment").val());
+    const quest2 = parseInt($("#comply").val());
+    const quest3 = parseInt($("#reason").val());    
+    const quest4 = parseInt($("#pokemon").val());
+    const quest5 = parseInt($("#vacation").val());
+    const total = quest1 + quest2 + quest3 + quest4 + quest5;
+
+    if(total)
+  })
+})
