c2ef39f7ad1e346152147becea9ab20ee76eb3c9
Cesar Lopez <maclopez88@yahoo.com>
2021-11-06 22:34:49 -0700

    add functional if else statements in js as well as .scrollTop method to show hidden div when called without having to scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,17 @@ $(document).ready(function() {
     const quest5 = parseInt($("#vacation").val());
     const total = quest1 + quest2 + quest3 + quest4 + quest5;
     
-    if(total >= 5) {
+    if(total <= 5) {
       let resultVal = "Python";
       $("#language-suggested").text(resultVal);
-    } else if (total <= 6 && total >= 10) {
+    } else if (total >= 6 && total <= 10) {
       let resultVal = "Swift";
       $("#language-suggested").text(resultVal);
     } else {
       let resultVal = "Ruby";
       $("#language-suggested").text(resultVal);
     }
-      $("#result").show();
-    
+    $("#result").show();
+    $('html, body').scrollTop($(document).height());
   });
 });
-
-
-
-// if(total >= 5) {
-//   let resultVal = "Python";
-//   $("#language-suggested").text(resultVal);
-//   } else if (total <= 6 && total >= 10) {
-//     let resultVal = "Swift";
-//     $("#language-suggested").text(resultVal);
-//   } else {
-//     let resultVal = "Ruby";
-//     $("#language-suggested").text(resultVal);
-//   }
-//   $("#result").show();
